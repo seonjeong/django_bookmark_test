2be82c5652d80e755ea3bd5ac343a4fdbdcb133f
YSJ <theblesedcity@hanmail.net>
2019-10-30 14:50:42 +0900

    배포 설정

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=j^+(#6s75mo6s(9))!f#7n9#yohuv6@3#ham16dm!cehy@70f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
